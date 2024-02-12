f781e6fc5211d42f95aa906289ce00850bcaaf74
karthis17 <karthirs602@gmail.com>
2024-02-12 18:23:57 +0530

    quizze was added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 const userRouter = require("./router/user");
 const imageRouter = require("./router/post");
 const pollRouter = require("./router/poll");
-const im = require("./router/quizzes");
+const quizzeRouter = require("./router/quizzes");
 const flamesRouter = require("./router/flames");
 const frameRouter = require("./router/frams");
 const cors = require("cors")
@@ -30,7 +30,7 @@ app.use("/api/img", imageRouter);
 app.use("/api/poll", pollRouter);
 app.use("/api/frame", frameRouter);
 app.use("/api/flames", flamesRouter);
-app.use("/img", im);
+app.use("/api/quizzes", quizzeRouter);
 
 mongoose.connect(process.env.MONG_URL).then(() => {
     app.listen(port, () => {
