5f359cb53980e34246c5f0365b87f95093574f08
Jabez38 <jabezraja3111997@gmail.com>
2024-03-03 15:32:43 +0530

    friend love and random img and frd love calc

--- model/randomImage.model.js
+++ model/randomImage.model.js
@@ -3,7 +3,6 @@ const { default: mongoose } = require("mongoose");
 module.exports = mongoose.model('randomImage', {
 
     frameUrl: { type: String, required: true },
-    framePath: { type: String, required: true },
     frameName: { type: String, required: true },
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
     shares: { type: Number, default: 0 },
